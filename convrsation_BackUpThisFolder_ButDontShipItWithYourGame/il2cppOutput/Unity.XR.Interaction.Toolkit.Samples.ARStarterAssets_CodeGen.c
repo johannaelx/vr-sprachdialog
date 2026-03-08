c18232a578cfa647aa0da6d2e8f3b38f6b2d390d
Victoria Gassner <victoria.gassner@web.de>
2026-03-08 12:44:28 +0100

    Updated menu

---
+++ convrsation_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Unity.XR.Interaction.Toolkit.Samples.ARStarterAssets_CodeGen.c
@@ -0,0 +1,42 @@
+﻿#include "pch-c.h"
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+extern void UnitySourceGeneratedAssemblyMonoScriptTypes_v1_Get_mE53D95EFE4CC954EAE5ECFB8EA7F3C3090EA5FFD (void);
+extern void UnitySourceGeneratedAssemblyMonoScriptTypes_v1__ctor_m6E92536916FCFE815DCC96DCC979354D4655AD0E (void);
+static Il2CppMethodPointer s_methodPointers[2] = 
+{
+	UnitySourceGeneratedAssemblyMonoScriptTypes_v1_Get_mE53D95EFE4CC954EAE5ECFB8EA7F3C3090EA5FFD,
+	UnitySourceGeneratedAssemblyMonoScriptTypes_v1__ctor_m6E92536916FCFE815DCC96DCC979354D4655AD0E,
+};
+static const int32_t s_InvokerIndices[2] = 
+{
+	24322,
+	14077,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_Unity_XR_Interaction_Toolkit_Samples_ARStarterAssets_CodeGenModule;
+const Il2CppCodeGenModule g_Unity_XR_Interaction_Toolkit_Samples_ARStarterAssets_CodeGenModule = 
+{
+	"Unity.XR.Interaction.Toolkit.Samples.ARStarterAssets.dll",
+	2,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+};
